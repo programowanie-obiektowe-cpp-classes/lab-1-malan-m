896c5809d6c18fb0ff828184b423f080f36a32a2
Michał Malankiewicz <malankiewicz.m@gmail.com>
2024-10-22 10:25:52 +0200

    Lab_1 rozwiazane

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,24 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    private:
+        double x, y;
+
+    public:
+    Wektor2D() : x(0), y(0) {}
+
+    Wektor2D(double x, double y) : x(x), y(y) {}
+
+    void setX(double x) { this->x = x; }
+    void setY(double y) { this->y = y; }
+
+    double getX() const { return x; }
+    double getY() const { return y; }
+
+    Wektor2D operator+(const Wektor2D& other) const {
+        return Wektor2D(x + other.x, y + other.y);
+    }
+
+    double operator*(const Wektor2D& other) const {
+        return (x * other.x + y * other.y);
+    }
 };
